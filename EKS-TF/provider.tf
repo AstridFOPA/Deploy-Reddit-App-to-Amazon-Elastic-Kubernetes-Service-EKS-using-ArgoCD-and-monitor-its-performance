8e7f01b284d821b519b24f6d04adaf04c58c04f0
Astrid FOPA <moisefopa@gmail.com>
2026-02-18 15:42:03 -0700

    new

--- EKS-TF/provider.tf
+++ EKS-TF/provider.tf
@@ -3,11 +3,11 @@ provider "aws" {
 }
 
 data "aws_eks_cluster" "this" {
-  name = aws_eks_cluster.this.name
+  name = var.cluster-name.this.name
 }
 
 data "aws_eks_cluster_auth" "this" {
-  name = aws_eks_cluster.this.name
+  name = var.cluster-name.this.name
 }
 
 provider "kubernetes" {
