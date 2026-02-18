bb5ef42d797b7925af05495cfaa0fc06d4e2734d
Astrid FOPA <moisefopa@gmail.com>
2026-02-18 15:36:50 -0700

    magnaging role

---
+++ EKS-TF/aws-auth.tf
@@ -0,0 +1,20 @@
+resource "kubernetes_config_map" "aws_auth" {
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+    mapRoles = yamlencode([
+      {
+        rolearn  = data.aws_iam_role.jenkins_role.arn
+        username = "jenkins"
+        groups   = ["system:masters"]
+      }
+    ])
+  }
+}
+
+data "aws_iam_role" "jenkins_role" {
+  name = "Jenkins-iam-role"
+}
--- Jenkins-server-TF/iam-instance-profile.tf
+++ Jenkins-server-TF/iam-instance-profile.tf
@@ -2,3 +2,9 @@ resource "aws_iam_instance_profile" "instance-profile" {
   name = "Jenkins-instance-profile"
   role = aws_iam_role.iam-role.name
 }
+
+
+
+
+
+arn:aws:sts::864899864155:role/Jenkins-iam-role/i-0aaeee82ff4a71f57
